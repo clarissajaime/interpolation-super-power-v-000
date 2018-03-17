dca33639c82979e3204e8c00f3e7d9bc61815c0e
Clarissa Jaime <clarissanjaime@gmail.com>
2018-03-17 18:27:14 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,6 @@
 # Write your #display_rainbow method here
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-colors.take(7)
 
 def display_rainbow(colors)
-  puts colors
+  puts colors.take(7)
 end
